9751ff754312ec7beab48330e0cc5d826c171569
ethichadebe <ethichadebe@gmail.com>
2025-08-01 12:07:06 +0200

    Scraper fix

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import scrapeSite from './scraper.js';
+import scrapeSite from './scraper.mjs';
 import cors from 'cors';
 
 const app = express();
